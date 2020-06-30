e68c8dc824fe3a0691fbf4d51482d6dd792d34cc
ghokun <gokhun@gmail.com>
2020-06-30 23:44:42 +0300

    working sample

---
+++ template/quarkus-native/function/src/test/java/io/github/ghokun/NativeOpenFaasFunctionIT.java
@@ -0,0 +1,9 @@
+package io.github.ghokun;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeOpenFaasFunctionIT extends OpenFaasFunctionTest {
+
+    // Execute the same tests but in native mode.
+}
